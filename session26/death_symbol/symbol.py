058f6107015834c09ac8411ad235fe0a5b644368
najmeh <najmehkhani.95@gmail.com>
2024-11-23 13:01:30 -0800

    update

--- session26/death_symbol/symbol.py
+++ session26/death_symbol/symbol.py
@@ -8,13 +8,13 @@ diagonal_length = 50
 thickness = 15  
 
 for i in range(diagonal_length):
-    for t in range(thickness):
-        if i + t < image.shape[1] and diagonal_length - i - 1 + t < image.shape[0]:
-            image[diagonal_length - i - 1 + t, i] = [0, 0, 0]  
-            image[diagonal_length - i - 1, i + t] = [0, 0, 0]  
-        if i - t >= 0 and diagonal_length - i - 1 - t >= 0:
-            image[diagonal_length - i - 1 - t, i] = [0, 0, 0]  
-            image[diagonal_length - i - 1, i - t] = [0, 0, 0]  
+    for j in range(thickness):
+        if i + j < image.shape[1] and diagonal_length - i - 1 + j < image.shape[0]:
+            image[diagonal_length - i - 1 + j, i] = [0, 0, 0]  
+            image[diagonal_length - i - 1, i + j] = [0, 0, 0]  
+        if i - j >= 0 and diagonal_length - i - 1 - j >= 0:
+            image[diagonal_length - i - 1 - j, i] = [0, 0, 0]  
+            image[diagonal_length - i - 1, i - j] = [0, 0, 0]  
 
 cv2.imwrite('session26/death_symbol/death_image.jpg', image)
 
