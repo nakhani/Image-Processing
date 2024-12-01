f72427c1689853c9eb4128092930ef171e4e654d
najmeh <najmehkhani.95@gmail.com>
2024-12-01 08:38:41 -0800

    update football pitch

--- Face Detection/football_pitch/football.py
+++ Face Detection/football_pitch/football.py
@@ -1,8 +1,20 @@
 import cv2
+import numpy as np
 
+background = np.ones((600, 897, 3), dtype=np.uint8)
 
-background = cv2.imread('football_pitch/istockphoto-1208398773-612x612.jpg')
-background = cv2.resize(background, (900, 600))
+light_green = (64, 128, 0)
+dark_green = (34, 139, 34)
+
+for i in range(7):
+    if i % 2 == 1:
+     color = light_green  
+    else:
+      color = dark_green
+
+    x_start = i * 128
+    x_end = x_start + 128
+    cv2.rectangle(background, (x_start, 0), (x_end, 600), color, -1)
 
 
 white = (255, 255, 255)
